509e1b8b52e364a135c56f11140dbef0feb2238a
Alexander <divinenickname@gmail.com>
2020-03-15 21:18:13 +0500

    Tostring override. Constructor changed

--- com.divinenickname/doctype/AbstractFileType.java
+++ com.divinenickname/doctype/AbstractFileType.java
@@ -3,9 +3,17 @@ package doctype;
 public abstract class AbstractFileType {
     private String data;
     private String name;
+    private int printTime;
+    private int letterHeight;
+    private int letterWidth;
+    private String type;
 
-    public AbstractFileType(String name) {
+    public AbstractFileType(String name, int printTime, int letterHeight, int letterWidth, String type) {
         this.name = name;
+        this.printTime = printTime;
+        this.letterHeight = letterHeight;
+        this.letterWidth = letterWidth;
+        this.type = type;
     }
 
     public String getData() {
@@ -22,5 +30,30 @@ public abstract class AbstractFileType {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPrintTime() {
+        return printTime;
+    }
+
+    private String formatter(String line, int length){
+        StringBuilder sb = new StringBuilder();
+        if(line.length()<length){
+            for (int i = 0; i < length - line.length(); i++) {
+                sb.append(" ");
+            }
+            line += sb.toString();
+            sb = new StringBuilder();
+        }
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        String delim = " | ";
+        type = formatter(type, 4);
+        String time = formatter(String.valueOf(printTime), 3);
+        name = formatter(name, 15);
+        return type + delim + time + delim + letterHeight + "x" + letterWidth + delim + name;
     }
 }
--- com.divinenickname/doctype/DOCXType.java
+++ com.divinenickname/doctype/DOCXType.java
@@ -1,8 +1,23 @@
 package doctype;
 
 public class DOCXType extends AbstractFileType {
-    public static final short PRINT_TIME = 25;
+    /**
+     * Time for print, seconds
+     */
+    public static final short PRINT_TIME = 15;
+
+    /**
+     * File extension type
+     */
+    public static final String TYPE = "docx";
+
+    /**
+     * A5 format
+     */
+    public static final int LETTER_HEIGHT = 210;
+    public static final int LETTER_WIDTH = 148;
+
     public DOCXType(String name) {
-        super(name);
+        super(name, PRINT_TIME, LETTER_HEIGHT, LETTER_WIDTH, TYPE);
     }
 }
