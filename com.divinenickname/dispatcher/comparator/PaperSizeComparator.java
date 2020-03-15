07cf761723bb7623213bb36bc4f88d88284910cc
Alexander <divinenickname@gmail.com>
2020-03-15 23:40:49 +0500

    renamed field

--- com.divinenickname/dispatcher/comparator/PaperSizeComparator.java
+++ com.divinenickname/dispatcher/comparator/PaperSizeComparator.java
@@ -10,8 +10,8 @@ import java.util.Comparator;
 public class PaperSizeComparator implements Comparator<AbstractFileType> {
     @Override
     public int compare(AbstractFileType o1, AbstractFileType o2) {
-        int size1 = o1.getLetterHeight() * o1.getLetterWidth();
-        int size2 = o2.getLetterHeight() * o2.getLetterWidth();
+        int size1 = o1.getPaperHeight() * o1.getPaperWidth();
+        int size2 = o2.getPaperHeight() * o2.getPaperWidth();
 
         return Integer.compare(size1, size2);
     }
